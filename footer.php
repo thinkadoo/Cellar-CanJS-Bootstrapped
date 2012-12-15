4b884d29fe4d55360625ffe3b062c8d15b49c829
thinkadoo <andre.n.venter@gmail.com>
2012-12-15 19:47:00 +0200

    split html into header footer and index

---
+++ footer.php
@@ -0,0 +1,31 @@
+<hr>
+
+<footer>
+    <p>&copy; Company 2012</p>
+</footer>
+
+</div><!--/.fluid-container-->
+
+
+<!-- Le javascript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery-1.7.1.min.js"></script>
+<script src="js/bootstrap-transition.js"></script>
+<script src="js/bootstrap-alert.js"></script>
+<script src="js/bootstrap-modal.js"></script>
+<script src="js/bootstrap-dropdown.js"></script>
+<script src="js/bootstrap-scrollspy.js"></script>
+<script src="js/bootstrap-tab.js"></script>
+<script src="js/bootstrap-tooltip.js"></script>
+<script src="js/bootstrap-popover.js"></script>
+<script src="js/bootstrap-button.js"></script>
+<script src="js/bootstrap-collapse.js"></script>
+<script src="js/bootstrap-carousel.js"></script>
+<script src="js/bootstrap-typeahead.js"></script>
+<script src="js/can.jquery.js"></script>
+<script src="js/can.fixture.js"></script>
+<script src="js/application.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require(header.php"); ?>
+
+
+    <div class="row-fluid">
+        <div class="hero-unit">
+            <h1>Your Cellar</h1>
+            <p>This is large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        </div>
+    </div>
+
+    <div class="row-fluid" id="wines" ><!--/The Wines List and Details Views are injected here-->
+
+    </div><!--/row-->
+
+<?php require(footer.php"); ?>
