2a6a00d5ebf7d562c68b3df28d747092d20e091c
thinkadoo <andre.n.venter@gmail.com>
2012-12-24 18:16:25 +0200

    replace the Cellar theme to match API views theme

--- header.php
+++ header.php
@@ -2,23 +2,83 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Cellar</title>
+    <title>Cellar &middot; Application</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="../../assets/css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
         body {
-            padding-top: 60px;
-            padding-bottom: 40px;
+            padding-top: 20px;
+            padding-bottom: 60px;
         }
-        .sidebar-nav {
-            padding: 9px 0;
+
+            /* Custom container */
+        .container {
+            margin: 0 auto;
+            max-width: 1000px;
+        }
+        .container > hr {
+            margin: 60px 0;
+        }
+
+            /* Main marketing message and sign up button */
+        .jumbotron {
+            margin: 80px 0;
+            text-align: center;
+        }
+        .jumbotron h1 {
+            font-size: 100px;
+            line-height: 1;
+        }
+        .jumbotron .lead {
+            font-size: 24px;
+            line-height: 1.25;
+        }
+        .jumbotron .btn {
+            font-size: 21px;
+            padding: 14px 24px;
+        }
+
+            /* Supporting marketing content */
+        .marketing {
+            margin: 60px 0;
+        }
+        .marketing p + h4 {
+            margin-top: 28px;
+        }
+
+
+            /* Customize the navbar links to be fill the entire space of the .navbar */
+        .navbar .navbar-inner {
+            padding: 0;
+        }
+        .navbar .nav {
+            margin: 0;
+        }
+        .navbar .nav li {
+            display: table-cell;
+            width: 1%;
+            float: none;
+        }
+        .navbar .nav li a {
+            font-weight: bold;
+            text-align: center;
+            border-left: 1px solid rgba(255,255,255,.75);
+            border-right: 1px solid rgba(0,0,0,.1);
+        }
+        .navbar .nav li:first-child a {
+            border-left: 0;
+            border-radius: 3px 0 0 3px;
+        }
+        .navbar .nav li:last-child a {
+            border-right: 0;
+            border-radius: 0 3px 3px 0;
         }
     </style>
-    <link href="css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="../../assets/css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -26,32 +86,40 @@
     <![endif]-->
 
     <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="ico/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../../assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.png">
 </head>
 
 <body>
 
-<div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="navbar-inner">
-        <div class="container">
-            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </a>
-            <a class="brand" href="#">Cellar</a>
-            <div class="nav-collapse collapse">
-                <p class="navbar-text pull-right">
-                </p>
-                <ul class="nav">
-                </ul>
-            </div><!--/.nav-collapse -->
-        </div>
-    </div>
-</div>
-
 <div class="container">
+
+    <div class="masthead">
+        <h3 class="muted">Cellar Application</h3>
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div class="container">
+                    <ul class="nav">
+                        <li class="active"><a href="">Home</a></li>
+                        <li><a href="api/data.php">Data</a></li>
+                        <li><a href="api/services.php">Services</a></li>
+                        <li><a href="api/downloads.php">Downloads</a></li>
+                        <li><a href="api/about.php">About</a></li>
+                        <li><a href="api/contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div><!-- /.navbar -->
+    </div>
+
+    <!-- Jumbotron -->
+    <div class="jumbotron">
+        <h1>Cellar</h1>
+        <p class="lead">Cellar is a CanJS MVC App with a RESTful API backend. </p>
+        <a class="btn btn-large btn-primary" href="./api/">Slim server API</a>
+    </div>
+
+    <hr>
--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php require("header.php"); ?>
 
 
-    <div class="row-fluid">
-        <div class="hero-unit">
-            <h1>Your Cellar</h1>
-            <p>This is large callout called the hero unit and three supporting pieces of content.</p>
-        </div>
-    </div>
-
     <div class="row-fluid" id="wines" ><!--/The Wines List and Details Views are injected here-->
 
     </div><!--/row-->
