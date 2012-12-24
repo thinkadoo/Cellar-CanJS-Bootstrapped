bddc00c1824cbe51f787db04535541f240d22c5a
thinkadoo <andre.n.venter@gmail.com>
2012-12-24 20:41:16 +0200

    content

--- index.php
+++ index.php
@@ -5,5 +5,27 @@
 
     </div><!--/row-->
 
+    <hr>
+
+    <h2>Appication</h2>
+
+    <div class="row-fluid">
+        <div class="span4">
+            <h3>CanJS</h3>
+            <p>CanJS is a MIT-licensed, client-side, JavaScript framework that makes building rich web applications easy. It's unique annotated style and OOP foundation lays the foundation for powerful MVC architectures. </p>
+            <p><a class="btn" href="http://canjs.us/" target="_blank">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+            <h3>jQuery</h3>
+            <p>jQuery is a fast and concise JavaScript Library that simplifies HTML document traversing, event handling, animating, and Ajax interactions for rapid web development. jQuery is designed to change the way that you write JavaScript. </p>
+            <p><a class="btn" href="http://jquery.com/" target="_blank">View details &raquo;</a></p>
+        </div>
+        <div class="span4">
+            <h3>Bootstrap</h3>
+            <p>Sleek, intuitive, and powerful front-end framework for faster and easier web development. </p>
+            <p><a class="btn" href="http://twitter.github.com/bootstrap/" target="_blank">View details &raquo;</a></p>
+        </div>
+    </div>
+
 
 <?php require("footer.php"); ?>
