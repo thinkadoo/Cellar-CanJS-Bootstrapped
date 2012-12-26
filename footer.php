803ffe44b60014fd403653c57a23377581fa8a99
thinkadoo <andre.n.venter@gmail.com>
2012-12-26 12:03:45 +0200

    cleanup

--- footer.php
+++ footer.php
@@ -10,21 +10,21 @@
 <!-- Le javascript
 ================================================== -->
 <!-- Placed at the end of the document so the pages load faster -->
-<script src="js/lib/jquery-1.7.2.min.js"></script>
-<script src="js/lib/bootstrap-transition.js"></script>
-<script src="js/lib/bootstrap-alert.js"></script>
-<script src="js/lib/bootstrap-modal.js"></script>
-<script src="js/lib/bootstrap-dropdown.js"></script>
-<script src="js/lib/bootstrap-scrollspy.js"></script>
-<script src="js/lib/bootstrap-tab.js"></script>
-<script src="js/lib/bootstrap-tooltip.js"></script>
-<script src="js/lib/bootstrap-popover.js"></script>
-<script src="js/lib/bootstrap-button.js"></script>
-<script src="js/lib/bootstrap-collapse.js"></script>
-<script src="js/lib/bootstrap-carousel.js"></script>
-<script src="js/lib/bootstrap-typeahead.js"></script>
-<script src="js/lib/can.jquery-1.1.3.js"></script>
-<script src="js/application.js"></script>
+<script src="./js/lib/jquery-1.7.2.min.js"></script>
+<script src="./js/lib/bootstrap-transition.js"></script>
+<script src="./js/lib/bootstrap-alert.js"></script>
+<script src="./js/lib/bootstrap-modal.js"></script>
+<script src="./js/lib/bootstrap-dropdown.js"></script>
+<script src="./js/lib/bootstrap-scrollspy.js"></script>
+<script src="./js/lib/bootstrap-tab.js"></script>
+<script src="./js/lib/bootstrap-tooltip.js"></script>
+<script src="./js/lib/bootstrap-popover.js"></script>
+<script src="./js/lib/bootstrap-button.js"></script>
+<script src="./js/lib/bootstrap-collapse.js"></script>
+<script src="./js/lib/bootstrap-carousel.js"></script>
+<script src="./js/lib/bootstrap-typeahead.js"></script>
+<script src="./js/lib/can.jquery-1.1.3.js"></script>
+<script src="./js/application.js"></script>
 
 </body>
 </html>
--- header.php
+++ header.php
@@ -8,9 +8,9 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href="css/lib/bootstrap.css" rel="stylesheet">
-    <link href="css/application.css" rel="stylesheet">
-    <link href="css/lib/bootstrap-responsive.css" rel="stylesheet">
+    <link href="./css/lib/bootstrap.css" rel="stylesheet">
+    <link href="./css/application.css" rel="stylesheet">
+    <link href="./css/lib/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -18,11 +18,11 @@
     <![endif]-->
 
     <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-57-precomposed.png">
-    <link rel="shortcut icon" href="ico/favicon.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="./ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="./ico/favicon.png">
 </head>
 
 <body>
