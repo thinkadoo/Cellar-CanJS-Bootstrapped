a94a220a44e79af5acf34683e13b9fc644bcb673
thinkadoo <andre.n.venter@gmail.com>
2012-12-26 16:27:45 +0200

    Issue #4	: Redirect to API Docs on non-Ajax request

--- api/functions/api.php
+++ api/functions/api.php
@@ -8,49 +8,81 @@
  */
 
 function getWines() {
-    $w = new Wine();
-    $result = $w->getWines();
-    echo $result;
+    global $app;
+    if($app->request()->isAjax()){
+        $w = new Wine();
+        $result = $w->getWines();
+        echo $result;
+    }else{
+        redirectToWelcome();
+    }
 }
-
 function getWine($id) {
-    $w = new Wine();
-    $result = $w->getWine($id);
-    echo $result;
+    global $app;
+    if($app->request()->isAjax()){
+        $w = new Wine();
+        $result = $w->getWine($id);
+        echo $result;
+    }else{
+        redirectToWelcome();
+    }
 }
 
 function addWine() {
     global $app;
-    $w = new Wine();
-    $request = $app->request();
-    $wine = ($request->getBody());
-    $utilities = new Utilities();
-    $requestArray = $utilities->urldecode_to_array($wine);
-    $requestObject = $utilities->array_to_object($requestArray);
-    $result = $w->addWine($requestObject);
-    echo $result;
+    if($app->request()->isAjax()){
+        $w = new Wine();
+        $request = $app->request();
+        $wine = ($request->getBody());
+        $utilities = new Utilities();
+        $requestArray = $utilities->urldecode_to_array($wine);
+        $requestObject = $utilities->array_to_object($requestArray);
+        $result = $w->addWine($requestObject);
+        echo $result;
+    }else{
+        redirectToWelcome();
+    }
 }
 
 function updateWine($id) {
     global $app;
-    $w = new Wine();
-    $request = $app->request();
-    $wine = ($request->getBody());
-    $utilities = new Utilities();
-    $requestArray = $utilities->urldecode_to_array($wine);
-    $requestObject = $utilities->array_to_object($requestArray);
-    $result = $w->updateWine($requestObject,$id);
-    echo $result;
+    if($app->request()->isAjax()){
+        $w = new Wine();
+        $request = $app->request();
+        $wine = ($request->getBody());
+        $utilities = new Utilities();
+        $requestArray = $utilities->urldecode_to_array($wine);
+        $requestObject = $utilities->array_to_object($requestArray);
+        $result = $w->updateWine($requestObject,$id);
+        echo $result;
+    }else{
+        redirectToWelcome();
+    }
 }
 
 function deleteWine($id) {
-    $w = new Wine();
-    $result = $w->deleteWine($id);
-    echo $result;
+    global $app;
+    if($app->request()->isAjax()){
+        $w = new Wine();
+        $result = $w->deleteWine($id);
+        echo $result;
+    }else{
+        redirectToWelcome();
+    }
 }
 
 function findByName($query) {
-    $w = new Wine();
-    $result = $w->findByName($query);
-    echo $result;
+    global $app;
+    if($app->request()->isAjax()){
+        $w = new Wine();
+        $result = $w->findByName($query);
+        echo $result;
+    }else{
+        redirectToWelcome();
+    }
+}
+
+function redirectToWelcome(){
+    global $app;
+    $app->redirect($app->request()->getRootUri()."/");
 }
