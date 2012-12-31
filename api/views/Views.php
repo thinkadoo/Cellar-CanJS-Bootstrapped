580a466c674e0a3654d57a00ea2bfff99e0d6060
thinkadoo <andre.n.venter@gmail.com>
2012-12-31 13:50:04 +0200

    add dynmanic menu bar to API views

--- api/views/Views.php
+++ api/views/Views.php
@@ -16,13 +16,16 @@ class Views
         $this->twig = $Twig;
     }
 
-    function welcome() {
+    public function welcome() {
         $template = $this->twig->loadTemplate('welcome.html');
-        $buffer = $template->render(array());
+        $navigation = $this->getMenuData();
+        // VARS passed to TWIG to change the persistent menu bar
+        $navigation[0] = array('href' => './', 'caption' => 'Home', 'class'=>'active');
+        $buffer = $template->render(array('navigation'=>$navigation));
         echo $buffer;
     }
 
-    function data() {
+    public function data() {
         $data = $this->wineModel->getWinesArray();
         $wines = array();
         $item = array();
@@ -39,34 +42,62 @@ class Views
             $item = null;
         }
         $template = $this->twig->loadTemplate('data.html');
-        $buffer = $template->render(array('wines' => $wines));
+        $navigation = $this->getMenuData();
+        // VARS passed to TWIG to change the persistent menu bar
+        $navigation[1] = array('href' => './services', 'caption' => 'Services', 'class'=>'active');
+        $buffer = $template->render(array('wines' => $wines, 'navigation'=>$navigation));
         echo $buffer;
     }
 
-    function services() {
+    public function services() {
         $wines = $this->wineModel->getWines();
         $wine = $this->wineModel->getWine(1);
         $template = $this->twig->loadTemplate('services.html');
-        $buffer = $template->render(array('wines' => $wines, 'wine' => $wine));
+        $navigation = $this->getMenuData();
+        // VARS passed to TWIG to change the persistent menu bar
+        $navigation[2] = array('href' => './data', 'caption' => 'Data', 'class'=>'active');
+        $buffer = $template->render(array('wines' => $wines, 'wine' => $wine, 'navigation'=>$navigation));
         echo $buffer;
     }
 
-    function downloads() {
+    public function downloads() {
         $template = $this->twig->loadTemplate('downloads.html');
-        $buffer = $template->render(array());
+        $navigation = $this->getMenuData();
+        // VARS passed to TWIG to change the persistent menu bar
+        $navigation[3] = array('href' => './downloads', 'caption' => 'Downloads', 'class'=>'active');
+        $buffer = $template->render(array('navigation'=>$navigation));
         echo $buffer;
     }
 
-    function about() {
+    public function about() {
         $template = $this->twig->loadTemplate('about.html');
-        $buffer = $template->render(array());
+        $navigation = $this->getMenuData();
+        // VARS passed to TWIG to change the persistent menu bar
+        $navigation[4] = array('href' => './about', 'caption' => 'About', 'class'=>'active');
+        $buffer = $template->render(array('navigation'=>$navigation));
         echo $buffer;
     }
 
-    function contact() {
+    public function contact() {
         $template = $this->twig->loadTemplate('contact.html');
-        $buffer = $template->render(array());
+        $navigation = $this->getMenuData();
+        // VARS passed to TWIG to change the persistent menu bar
+        $navigation[5] = array('href' => './contact', 'caption' => 'Contact', 'class'=>'active');
+        $buffer = $template->render(array('navigation'=>$navigation));
         echo $buffer;
+    }
+
+    private function getMenuData() {
+        // VARS passed to TWIG to build the persistent menu bar
+        $navigation = array(
+            array('href' => './', 'caption' => 'Home', 'class'=>''),
+            array('href' => './data', 'caption' => 'Data', 'class'=>''),
+            array('href' => './services', 'caption' => 'Services', 'class'=>''),
+            array('href' => './downloads', 'caption' => 'Downloads', 'class'=>''),
+            array('href' => './about', 'caption' => 'About', 'class'=>''),
+            array('href' => './contact', 'caption' => 'Contact', 'class'=>'')
+        );
+        return $navigation;
     }
 
 }
