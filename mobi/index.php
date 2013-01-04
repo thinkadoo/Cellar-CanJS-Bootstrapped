33279630e4941f547daf4b43cdbf4ccbafea6033
thinkadoo <andre.n.venter@gmail.com>
2013-01-04 11:15:25 +0200

    add lighter menu bar

--- mobi/index.php
+++ mobi/index.php
@@ -37,7 +37,7 @@
 
 <?php require("header.php"); ?>
 
-    <div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar navbar-fixed-top">
         <div class="navbar-inner">
             <div class="container">
                 <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
